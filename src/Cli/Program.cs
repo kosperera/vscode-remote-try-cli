7e98678c3c9bc9e0cb61cc76f3e842c2686bf4f2
kosalanuwan <kosala.nuwan@gmail.com>
2020-11-11 20:14:12 +0000

    cli: initial commit
    - add command-line-api
    - add DI for cmd-line
    - use generic host in dotnet standard

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,45 +1,98 @@
-﻿using System;
-using System.CommandLine;
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
-using System.IO;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Linq;
 
 namespace Weather.Cli
 {
-    class Program
+
+    public class Program
     {
-        static int Main(string[] args)
+        static async Task<int> Main(string[] args) => await CreateHostBuilder().BuildConsole().RunConsoleAsync(args);
+
+        private static IHostBuilder CreateHostBuilder() => Host.CreateDefaultBuilder().ConfigureServices(RegisterCommands);
+
+        private static void RegisterCommands(IServiceCollection services)
         {
-            Console.WriteLine("Using CommandLine");
-
-            RootCommand rootCmd = new RootCommand
-            {
-                new Option<int>
-                (
-                    "--int-option", getDefaultValue: () => 42, description: "An option whose argument is parsed as an int"
-                ),
-                new Option<bool>
-                (
-                    "--bool-option", "An option whose argument is parsed as a bool"
-                ),
-                new Option<FileInfo>
-                (
-                    "--file-option", "An option whose argument is parsed as a FileInfo"
-                )
-            };
-            rootCmd.Description = "Weather CLI";
-            // NOTE: The parameters of the handler method are matched according to the names of the options.
-            rootCmd.Handler = CommandHandler.Create<int, bool, FileInfo>
-            (
-                (intOption, boolOption, fileOption) => 
-                {
-                    Console.WriteLine($"The value for --int-option is: {intOption}");
-                    Console.WriteLine($"The value for --bool-option is: {boolOption}");
-                    Console.WriteLine($"The value for --file-option is: {fileOption?.FullName ?? "null"}");
-                }
-            );
-
-            // HINT: Parse the incoming args and invoke the handler.
-            return rootCmd.InvokeAsync(args).Result;
+            services.AddCommandsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddCommandLineBuilder();
         }
     }
+
+    public class Greeter : Command
+    {
+        public Greeter() : base(name: "greet", "$ dotnet run greet --name 'John Doe'")
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            AddOption(new Option<string>("--name")
+            {
+                Name = "name",
+                Description = "person to greet",
+                IsRequired = true
+            });
+
+            Handler = CommandHandler.Create<GreeterOptions>(Execute);
+        }
+
+        private int Execute(GreeterOptions options)
+        {
+            Console.WriteLine($"Greetings, {options.Name}");
+
+            return 0;
+        }
+    }
+
+    public class GreeterOptions
+    {
+        public string Name { get; }
+
+        public GreeterOptions(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal static class DependencyExtensions
+    {
+        public static IServiceCollection AddCommandLineBuilder(this IServiceCollection services) => services.AddSingleton<CommandLineBuilder>(new CommandLineBuilder());
+
+        public static IServiceCollection AddCommandsFromAssembly(this IServiceCollection services, Assembly assembly)
+        {
+            Type openType = typeof(Command);
+
+            assembly.GetExportedTypes()
+                    .Where((type) => !type.IsAbstract && !type.IsGenericTypeDefinition && type.BaseType == openType && openType.IsAssignableFrom(type))
+                    .ToList()
+                    .ForEach((type) => services.AddSingleton(openType, Activator.CreateInstance(type)));
+
+            return services;
+        }
+    }
+
+    internal static class HostingExtensions
+    {
+        public static IHost BuildConsole(this IHostBuilder hostBuilder) => hostBuilder.UseConsoleLifetime().Build();
+
+        public static Task<int> RunConsoleAsync(this IHost host, string[] args)
+        {
+            CommandLineBuilder cli = host.Services.GetRequiredService<CommandLineBuilder>();
+            foreach (Command cmd in host.Services.GetServices<Command>())
+            {
+                cli.AddCommand(cmd);
+            }
+            
+            return cli.UseDefaults().Build().InvokeAsync(args);
+        }
+    }
+
 }
